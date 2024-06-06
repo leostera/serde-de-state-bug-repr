7fa81542d7a0524936f226bb15fae0a3ec68459c
Leandro Ostera <leandro@abstractmachines.dev>
2024-06-06 14:21:09 +0200

    asdf

--- db2/lib/db2.ml
+++ db2/lib/db2.ml
@@ -1,19 +1,47 @@
-module DbCaml = struct
+
+
+(* TODO: move all the Connection.t contents (driver,config) back out into Silo.t *)
+(* this is the wrong abstraction because FakeDB can't reference itself *)
+(* so FakeDB cannot create a connection that holds a reference to the FakeDB module *)
+
+module rec DbCaml : sig
   module Connection : sig
-    type t
+    type 'config t = {
+      driver : (module DbCaml.Driver.Intf with type config = 'config);
+      config : 'config;
+    }
 
-    val make : unit -> t
-  end = struct
-    type t = unit
+    val make :
+      driver:(module DbCaml.Driver.Intf with type config = 'config) ->
+      config:'config ->
+      'config t
 
-    let make () = ()
+    val execute : 'config t -> string -> bytes
+  end
+
+  module Driver : sig
+    module type Intf = sig
+      type config
+
+      val connect : config -> config DbCaml.Connection.t
+    end
+  end
+end = struct
+  module Connection = struct
+    type 'config t = {
+      driver : (module DbCaml.Driver.Intf with type config = 'config);
+      config : 'config;
+    }
+
+    let make ~driver ~config = { config; driver }
+    let execute _t _query = Bytes.of_string "hello world"
   end
 
   module Driver = struct
     module type Intf = sig
       type config
 
-      val connect : config -> Connection.t
+      val connect : config -> config DbCaml.Connection.t
     end
   end
 end
@@ -25,16 +53,22 @@ module FakeDB = struct
 end
 
 module Silo = struct
-  type 'config t = {
-    driver : (module DbCaml.Driver.Intf with type config = 'config);
-    config : 'config;
-  }
+  type 'config t = { connection : 'config DbCaml.Connection.t }
 
-  let connect ~driver ~config = { driver; config }
+  let connect :
+      type config.
+      driver:(module DbCaml.Driver.Intf with type config = config) ->
+      config:config ->
+      config t =
+   fun ~driver:(module D) ~config ->
+    let connection = D.connect config in
+    { connection }
+
+  let query t query = DbCaml.Connection.execute t.connection query
 end
 
 module MyApp = struct
   let run () =
     let db = Silo.connect ~driver:(module FakeDB) ~config:{ port = 2112 } in
-    ()
+    Silo.query db "select * from users"
 end
